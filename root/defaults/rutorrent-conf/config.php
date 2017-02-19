eb3410cfc35f91f370c664efd949fec985a768f9
root <root@ns309786.ip-188-165-195.eu>
2017-02-19 18:35:46 +0100

    fix curl access

--- root/defaults/rutorrent-conf/config.php
+++ root/defaults/rutorrent-conf/config.php
@@ -41,7 +41,7 @@
 
 	$pathToExternals = array(
 		"php" 	=> '/usr/bin/php7',			// Something like /usr/bin/php. If empty, will be found in PATH.
-		"curl"	=> '/usr/bin/curl',			// Something like /usr/bin/curl. If empty, will be found in PATH.
+		"curl"	=> '/usr/local/bin/curl',			// Something like /usr/bin/curl. If empty, will be found in PATH.
 		"gzip"	=> '/usr/bin/gzip',			// Something like /usr/bin/gzip. If empty, will be found in PATH.
 		"id"	=> '/usr/bin/id',			// Something like /usr/bin/id. If empty, will be found in PATH.
 		"stat"	=> '/bin/stat',			// Something like /usr/bin/stat. If empty, will be found in PATH.
