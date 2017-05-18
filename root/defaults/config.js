c9ca4fba6edc6a4effa6df14b2b048816d1097b2
romancin <roman.martin84@gmail.com>
2017-05-18 21:50:34 +0200

    Merge from devel

--- root/defaults/config.js
+++ root/defaults/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-  baseURI: '/',
+  baseURI: '<CONTEXT_PATH>',
   dbCleanInterval: 1000 * 60 * 60,
   dbPath: '/config/flood-db/',
   floodServerPort: 3000,
   maxHistoryStates: 30,
   pollInterval: 1000 * 5,
-  secret: 'password',
+  secret: '<FLOOD_SECRET>',
   scgi: {
     host: 'localhost',
     port: 5000,
