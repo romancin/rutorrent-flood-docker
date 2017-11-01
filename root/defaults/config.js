7f0a3803144543e4286a0008350c8874384179a3
romancin <roman.martin84@gmail.com>
2017-11-01 08:51:36 +0100

    Added current flood version config parameters

--- root/defaults/config.js
+++ root/defaults/config.js
@@ -1,11 +1,13 @@
 const CONFIG = {
-  baseURI: '<CONTEXT_PATH>',
+  baseURI: '/',
   dbCleanInterval: 1000 * 60 * 60,
   dbPath: '/config/flood-db/',
+  floodServerHost: '127.0.0.1',
   floodServerPort: 3000,
+  floodServerProxy: 'http://127.0.0.1',
   maxHistoryStates: 30,
   pollInterval: 1000 * 5,
-  secret: '<FLOOD_SECRET>',
+  secret: 'flood',
   scgi: {
     host: 'localhost',
     port: 5000,
@@ -14,7 +16,8 @@ const CONFIG = {
   },
   ssl: true,
   sslKey: '/config/nginx/key.pem',
-  sslCert: '/config/nginx/cert.pem'
+  sslCert: '/config/nginx/cert.pem',
+  torrentClientPollInterval: 1000 * 2
 };
 
 module.exports = CONFIG;
