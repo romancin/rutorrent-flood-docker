c17cee26da9601d03bdbfb37416e2eef38e86fb1
Román Martín <roman.martin@me.com>
2019-03-17 23:51:57 +0100

    Upaste flood config file according to official

--- root/defaults/config.js
+++ root/defaults/config.js
@@ -2,10 +2,9 @@ const CONFIG = {
   baseURI: '/',
   dbCleanInterval: 1000 * 60 * 60,
   dbPath: '/config/flood-db/',
-  floodServerHost: '0.0.0.0',
   floodServerPort: 3000,
-  floodServerProxy: 'http://127.0.0.1:3000',
   maxHistoryStates: 30,
+  pollInterval: 1000 * 5,
   secret: 'flood',
   scgi: {
     host: 'localhost',
@@ -16,7 +15,6 @@ const CONFIG = {
   ssl: true,
   sslKey: '/config/nginx/key.pem',
   sslCert: '/config/nginx/cert.pem',
-  torrentClientPollInterval: 1000 * 2
 };
 
 module.exports = CONFIG;
